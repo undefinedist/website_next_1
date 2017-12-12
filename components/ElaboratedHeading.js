dee465355dea073d9492387ba7d7beb9061d0306
Eung Jin Lee <oiojin831@gmail.com>
2017-12-12 19:24:03 +0900

    change elaboratedheading color

--- components/ElaboratedHeading.js
+++ components/ElaboratedHeading.js
@@ -2,14 +2,22 @@ import { Text, Box } from "rebass";
 import Container from "./Container";
 import Prefix from "./Prefix";
 
-export default ({ mainColor, subColor, prefix, primary, surfix, align }) => {
+export default ({
+  prefixColor,
+  surfixColor,
+  primaryColor,
+  prefix,
+  primary,
+  surfix,
+  align
+}) => {
   return (
     <Box px={[3, 3, 4]} pb={[0, 4]}>
-      <Prefix color={subColor} text={prefix} align={align} />
-      <Text color={mainColor} mb="-8px" align={align} fontSize={[5]}>
+      <Prefix color={prefixColor} text={prefix} align={align} />
+      <Text color={primaryColor} mb="-8px" align={align} fontSize={[5]}>
         {primary}
       </Text>
-      <Prefix color={subColor} text={surfix} align={align} />
+      <Prefix color={surfixColor} text={surfix} align={align} />
     </Box>
   );
 };
